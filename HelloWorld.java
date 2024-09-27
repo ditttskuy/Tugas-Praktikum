c9077584aeaad74e548cb7179c6bbc3b63ae3064
ditttskuy <adytiatgr1563@gmail.com>
2024-09-27 10:08:29 +0700

    FirstCommit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld{
     public static void main(String[] args){
         System.out.println("Hello Jawa");
+        System.out.println("nama saya");
+        System.out.println("adytia");
     }
 }
